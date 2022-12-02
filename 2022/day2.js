bf4b54d768e7947eff0a350d6d16a78207a4f83e
Eddie Lim <eddie.lim@foodpanda.com>
2022-12-03 05:32:00 +0800

    day2 part2

--- 2022/day2.js
+++ 2022/day2.js
@@ -7,16 +7,27 @@ const LOST = "lost";
 const DRAW = "draw";
 const WON = "won";
 
-const mapChoice = new Map([
+const mapChoiceBase = new Map([
     ["A", ROCK],
     ["B", PAPER],
     ["C", SCISSORS],
+]);
+
+const mapChoicePart1 = new Map([
+    ...mapChoiceBase,
     ["X", ROCK],
     ["Y", PAPER],
     ["Z", SCISSORS],
 ]);
 
-var points = new Map([
+const mapChoicePart2 = new Map([
+    ...mapChoiceBase,
+    ["X", LOST],
+    ["Y", DRAW],
+    ["Z", WON],
+]);
+
+const points = new Map([
     [ROCK, 1],
     [PAPER, 2],
     [SCISSORS, 3],
@@ -25,7 +36,13 @@ var points = new Map([
     [WON, 6],
 ]);
 
-const mechanism = { // Enemy will be negative. (Format = Enemy + Me)
+const choices = {
+    1 : ROCK,
+    2 : PAPER,
+    3 : SCISSORS,
+}
+
+const mechanismPart1 = { // Enemy will be negative. (Format = Enemy + Me)
     "0" : DRAW,  // ROCK(-1) + ROCK(1) = 0. // PAPER(-2) + PAPER(2) = 0. // SCISSORS(-3) + SCISSORS(3) = 0.
     "1" : WON,   // ROCK(-1) + PAPER(2) = 1. ROCK lose to PAPER. // PAPER(-2) + SCISSORS(3) = 1. PAPER lose to SCISSORS.
     "-2" : WON,  // SCISSORS(-3) + ROCK(1) = -2. SCISSORS lose to ROCK.
@@ -33,21 +50,56 @@ const mechanism = { // Enemy will be negative. (Format = Enemy + Me)
     "-1" : LOST, // PAPER(-2) + ROCK(1) = -1. PAPER wins ROCK. // SCISSORS(-3) + PAPER(2) = -1. SCISSORS wins PAPER.
 };
 
+const mechanismPart2 = new Map([
+    [WON, new Map ([
+            [ROCK, PAPER],
+            [SCISSORS, ROCK],
+            [PAPER, SCISSORS],
+        ])
+    ],
+    [LOST, new Map ([
+            [ROCK, SCISSORS],
+            [PAPER, ROCK],
+            [SCISSORS, PAPER],
+        ])
+    ],  // ROCK(-1) + SCISSORS(3) = 2. ROCK wins SCISSORS.
+]);
+
 const part1 = (input) => {
     const round = input.split("\n");
     var totalScore = 0
     round.forEach(function(currentValue, i){
         const [enemy, me] = currentValue.split(" ");
-        const enemyChoice = mapChoice.get(enemy);
-        const myChoice = mapChoice.get(me);
-        const play = (-points.get(enemyChoice)) + (points.get(myChoice)) + ""
-        const result = mechanism[play];
+        const enemyChoice = mapChoicePart1.get(enemy);
+        const myChoice = mapChoicePart1.get(me);
+        const play = -points.get(enemyChoice) + points.get(myChoice)
+        const result = mechanismPart1[play + ""];
         totalScore += points.get(result) + points.get(myChoice);
     });
     console.log("day 2 - part 1:", totalScore);
 }
 
+const part2 = (input) => {
+    const round = input.split("\n");
+    var totalScore = 0
+    round.forEach(function(currentValue){
+        const [enemy, me] = currentValue.split(" ");
+        const enemyChoice = mapChoicePart2.get(enemy);
+        if(me == "Y"){
+            totalScore += points.get(DRAW) + points.get(enemyChoice)
+        } else {
+            const myResult = mapChoicePart2.get(me);
+            const myChoices = mechanismPart2.get(myResult);
+            const myPlay = myChoices.get(enemyChoice);
+            
+            totalScore += points.get(myResult) + points.get(myPlay) ;
+        }
+    });
+    console.log("day 2 - part 2:", totalScore);
+}
+
 export async function execute(){
     const input = await getDayInput(2)
     part1(input);
+    part2(input);
 }
