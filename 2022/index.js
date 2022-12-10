48c3b43e7b9f33f3852ce2c13f124c4bfde0579c
Eddie Lim <eddie.lim@foodpanda.com>
2022-12-10 22:34:47 +0800

    day 4 part 1

--- 2022/index.js
+++ 2022/index.js
@@ -1,9 +1,11 @@
 import { execute as day1 } from './day1.js';
 import { execute as day2 } from './day2.js';
 import { execute as day3 } from './day3.js';
+import { execute as day4 } from './day4.js';
 
 await day1()
 await day2()
 await day3()
+await day4()
 
 process.exit()
